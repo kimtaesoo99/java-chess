bd5fb4db6985ccee19321645f46a070c86730232
kimtaesoo <kimtaesoo7@naver.com>
2023-06-29 19:51:17 +0900

    test: 체스판을 초기화 테스트 추가

---
+++ src/test/java/service/InitBoardTest.java
@@ -0,0 +1,26 @@
+package service;
+
+import domain.Location;
+import domain.Pieces;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InitBoardTest {
+
+    @DisplayName("보드를 초기화할경우 기물이 32개가 설정된다.")
+    @Test
+    void getInitBoard() {
+        //given
+        InitBoard initBoard = new InitBoard();
+
+        //when
+        Map<Location, Pieces> board = initBoard.getInitBoard();
+
+        //then
+        assertThat(board.size()).isEqualTo(32);
+    }
+}
