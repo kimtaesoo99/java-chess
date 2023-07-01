3c0859b717c3d6d8f6db36f3dee0febf0118da48
kimtaesoo <kimtaesoo7@naver.com>
2023-07-01 20:49:24 +0900

    remove: 패키지 이동 및 로직 단순화

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,6 +1,6 @@
-package view;
+package chess.view;
 
-import dto.BoardStatusResponse;
+import chess.dto.BoardStatusResponse;
 
 public class OutputView {
 
@@ -8,9 +8,6 @@ public class OutputView {
     private static final String START_COMMAND = "> 게임 시작 : start";
     private static final String END_COMMAND = "> 게임 종료 : end";
     private static final String MOVE_COMMAND = "> 게임 이동 : move source 위치 target 위치 - 예. move b2 b3";
-    private static final int FIRST_LOCATION = 0;
-    private static final int RANK_LENGTH = 8;
-    private static final int ZERO = 0;
 
     public static void printStart() {
         System.out.println(START_GAME);
@@ -20,17 +17,7 @@ public class OutputView {
     }
 
     public static void printBoardStatus(final BoardStatusResponse boardStatusResponse) {
-        String boardStatus = boardStatusResponse.getBoardStatus();
-        for (int location = FIRST_LOCATION; location < boardStatus.length(); location++) {
-            if (isRankLength(location)) {
-                System.out.println();
-            }
-            System.out.print(boardStatus.charAt(location));
-        }
-    }
-
-    private static boolean isRankLength(final int location) {
-        return location != FIRST_LOCATION && location % RANK_LENGTH == ZERO;
+        System.out.println(boardStatusResponse.getBoardStatus());
     }
 
     public static void printErrorMessage(final String errorMessage) {
